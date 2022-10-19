8bda05b3de9518a266728c2dcb034949e17c01cb
fzunda <fernando.zunda@mercadolibre.com>
2022-10-19 13:00:36 -0300

    Ejercicio 1 Clase_1_1

---
+++ Go_Web/Clase_1_2/Ejercicio_1/go.mod
@@ -0,0 +1,3 @@
+module github.com/fzunda/backpack-bcgow6-agustin-zunda
+
+go 1.19
--- Go_Web/Clase_1_2/Ejercicio_1/main.go
+++ Go_Web/Clase_1_2/Ejercicio_1/main.go
@@ -15,6 +15,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/Go_Web/Clase_2_2/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,8 +45,54 @@ func GetAll(ctx *gin.Context) {
 	}
 }
 
-func searchTransaction(ctx *gin.Context) {
-	transaction, err 
+func searchTransaction() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		jsonData, err := os.ReadFile("./transactions.json")
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error:": err.Error()})
+		} else {
+			var arrayTransactions []transaction
+			err := json.Unmarshal([]byte(jsonData), &arrayTransactions)
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error:": err.Error()})
+			} else {
+				var req transaction
+				if err := ctx.ShouldBindJSON(&req); err != nil {
+					ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+					return
+				}
+
+				var result transaction
+
+				for _, val := range arrayTransactions {
+					if req.Id != 0 && val.Id == req.Id {
+						result = val
+					}
+					if req.Id != 0 && val.Id == req.Id && req.Code != "" && val.Code == req.Code {
+						result = val
+					}
+					if req.Id != 0 && val.Id == req.Id && req.Code != "" && val.Code == req.Code {
+						result = val
+					}
+					if req.Id != 0 && val.Id == req.Id && req.Code != "" && val.Code == req.Code && req.Coin != "" && val.Coin == req.Coin {
+						result = val
+					}
+					if req.Id != 0 && val.Id == req.Id && req.Code != "" && val.Code == req.Code && req.Coin != "" && val.Coin == req.Coin && req.Amount != 0 && val.Amount == req.Amount {
+						result = val
+					}
+					if req.Id != 0 && val.Id == req.Id && req.Code != "" && val.Code == req.Code && req.Coin != "" && val.Coin == req.Coin && req.Amount != 0 && val.Amount == req.Amount && req.Emitting != "" && val.Emitting == req.Emitting {
+						result = val
+					}
+					if req.Id != 0 && val.Id == req.Id && req.Code != "" && val.Code == req.Code && req.Coin != "" && val.Coin == req.Coin && req.Amount != 0 && val.Amount == req.Amount && req.Emitting != "" && val.Emitting == req.Emitting && req.Receptor != "" && val.Receptor == req.Receptor {
+						result = val
+					}
+				}
+				ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, result, ""))
+				return
+			}
+		}
+
+	}
 }
 
 func main() {
@@ -53,5 +100,6 @@ func main() {
 	router.GET("/transactions", func(c *gin.Context) {
 		GetAll(c)
 	})
+	router.GET("/transactions/search", searchTransaction())
 	router.Run()
 }
